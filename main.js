51515eee089d78181369ddcaf4061ab91afe5e82
javohirbro <javohir@felix-its.uz>
2024-12-05 13:39:50 +0500

    FEATURE / added regions

--- main.js
+++ main.js
@@ -1,17 +1,110 @@
 const {app, Tray, Menu, nativeImage, Notification} = require('electron/main');
 require('dotenv').config();
 
+let store;
+
+const initStore = async () => {
+    const ElectronStore = await import('electron-store');
+    store = new ElectronStore.default();
+    return store;
+};
+
 let tray;
 let fetchedData = {};
 let lastNotifiedNamaz = null;
 const notificationLeadTime = 10;
 
-app.on("ready", () => {
+// const regionsWithDistricts = {
+//     "Toshkent Viloyati": [
+//         "Toshkent", "Bekоbоd", "Оlоt", "Yangibоzоr", "Angren", "Chirchiq"
+//     ],
+//     "Samarqand Viloyati": [
+//         "Samarqand", "Kattaqo'rg'оn", "Urgut", "Zarafshоn"
+//     ],
+//     "Farg'ona Viloyati": [
+//         "Farg'оna", "Marg'ilon", "Qo'qon", "Quva", "Rishton", "Shahrixon"
+//     ],
+//     "Namangan Viloyati": [
+//         "Namangan", "Chоrt", "Pоp", "Uchqo'rg'оn", "Kosonsoy"
+//     ],
+//     "Buxoro Viloyati": [
+//         "Buxoro", "Qоrоvulbоzоr", "Gazli", "Kоnimех"
+//     ],
+//     "Xorazm Viloyati": [
+//         "Urganch", "Xiva", "Shоvоt", "Xazоrasp"
+//     ],
+//     "Qashqadaryo Viloyati": [
+//         "Qarshi", "G'uzоr", "Dehqоnоbоd", "Kоsоn", "Muborak"
+//     ],
+//     "Surxondaryo Viloyati": [
+//         "Termiz", "Denоv", "Sherоbоd", "Bоysun", "Qumqo'rg'оn"
+//     ],
+//     "Navoiy Viloyati": [
+//         "Navoiy", "Nurоta", "Uchquduq", "Kеnimех"
+//     ],
+//     "Jizzax Viloyati": [
+//         "Jizzах", "Arnasоy", "Zоmin", "Bоystоn"
+//     ],
+//     "Qoraqalpog'iston Respublikasi": [
+//         "Nukus", "Mo'ynоq", "Taxtako'pir", "Xo'jayli", "Qo'ng'irоt"
+//     ]
+// };
+
+const regionsWithDistricts = {
+    "Toshkent Viloyati": [
+        "Toshkent", "Angren"
+    ],
+    "Samarqand Viloyati": [
+        "Samarqand",  "Urgut"
+    ],
+    "Farg'ona Viloyati": [
+        "Marg'ilon", "Qo'qon", "Quva", "Rishton", "Shahrixon"
+    ],
+    "Namangan Viloyati": [
+        "Namangan", "Kosonsoy"
+    ],
+    "Buxoro Viloyati": [
+        "Buxoro", "Gazli"
+    ],
+    "Xorazm Viloyati": [
+        "Urganch", "Xiva"
+    ],
+    "Qashqadaryo Viloyati": [
+        "Qarshi", "Muborak"
+    ],
+    "Surxondaryo Viloyati": [
+        "Termiz"
+    ],
+    "Navoiy Viloyati": [
+        "Navoiy", "Uchquduq"
+    ],
+    "Qoraqalpog'iston Respublikasi": [
+        "Nukus"
+    ]
+};
+
+app.on("ready", async () => {
+    await initStore();
+
+    let currentRegion = store.get('selectedRegion', 'Toshkent');
+    let currentDistrict = store.get('selectedDistrict', 'Toshkent') ;
+
     const icon = nativeImage.createFromDataURL('');
     tray = new Tray(icon);
 
+    if (!currentRegion || !Object.keys(regionsWithDistricts).includes(currentRegion)) {
+        currentRegion = 'Toshkent Viloyati';
+        currentDistrict = 'Toshkent';
+
+        store.set('selectedRegion', currentRegion);
+        store.set('selectedDistrict', currentDistrict);
+    }
+
     const namazTime = () => {
-        fetch(`https://islomapi.uz/api/present/day?region=Toshkent`)
+        const requestUrl = `https://islomapi.uz/api/present/day?region=${currentDistrict}`;
+        console.log('Request URL:', requestUrl);
+
+        fetch(requestUrl)
             .then(res => res.json())
             .then(data => {
                 console.log(data);
@@ -96,10 +189,23 @@ app.on("ready", () => {
             },
             {type: 'separator'},
             {
-                label: "Hudud", submenu: [
-                    {label: "Namangan", type: "checkbox"},
-                    {label: "Toshkent", type: "checkbox"},
-                ]
+                label: "Hudud",
+                submenu: Object.entries(regionsWithDistricts).map(([region, districts]) => ({
+                    label: region,
+                    submenu: districts.map(district => ({
+                        label: district,
+                        type: "radio",
+                        checked: district === currentDistrict,
+                        click: () => {
+                            currentDistrict = district;
+                            currentRegion = region;
+
+                            store.set('selectedRegion', region);
+                            store.set('selectedDistrict', district);
+                            namazTime();
+                        }
+                    }))
+                }))
             },
             {type: 'separator'},
             {label: 'Chiqish', role: 'quit'}
@@ -110,14 +216,14 @@ app.on("ready", () => {
     tray.on('click', () => {
         new Notification({
             title: 'Namaz Reminder',
-            body: 'It\'s time for the next Namaz.'
+            body: `Next Namaz times for ${currentDistrict}`
         }).show();
     });
 
     namazTime();
     setInterval(updateTime, 1000);
 
-    tray.setToolTip('Namaz Time by Felix-ITS');
+    tray.setToolTip('Namoz Vaqti by @JavohirTech');
 });
 
 app.dock.hide();
